aafc8dd70784b8423ce170b490429c79c00163ad
Yousef Suliman <yousef.f.suliman@gmail.com>
2021-02-27 20:59:00 +0300

    Made working useReducer for a counter, started working towards combining it with useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { TextField } from './TextField';
+import React, { useReducer } from 'react';
+
+const initialCount: counterState = { count: 0 };
+
+const increaseAction = { type: 'increment' };
+const decreaseAction = { type: 'decrement' };
+
+type counterAction = typeof increaseAction | typeof decreaseAction;
+interface counterState {
+  count: number;
+};
+
+const counterReducer = (state: counterState, action: counterAction) => {
+  const prevState = { ...state }
+  switch (action.type) {
+    case 'increment':
+      return {
+        ...state, count: state.count + 1
+      }
+    case 'decrement':
+      return {
+        ...state, count: state.count - 1
+      }
+    default: return state
+  };
+}
+
+const counterContext = React.createContext<null | [state: counterState, dispatch: React.Dispatch<counterAction>]>(null);
+
 
 const App: React.FC = () => {
+  const [state, dispatch] = useReducer(counterReducer, initialCount)
+
   return (
-    <div className="App">
-      <p>yo</p>
-      <TextField text="text" person={{ firstName: 'sdf', lastName: 'sdf' }} />
+    <div>
+      <counterContext.Provider value={[state, dispatch]}>
+        <p>The count is {state.count}</p>
+        <button onClick={() => dispatch(increaseAction)}> + </button>
+        <button onClick={() => dispatch(decreaseAction)}> - </button>
+      </counterContext.Provider>
     </div>
   );
 };
